2b6e1d50c6af924a90be3731d60e00a04cd6a0cc
Gerrit Proessl <gerrit.prossl@tietoevry.com>
2023-07-08 11:25:26 +0200

    [Add] clang format | also lost nvim changes

---
+++ nvim/lua/neoformat_setup.lua
@@ -0,0 +1,6 @@
+vim.g.neoformat_cpp_clangformat = {
+  exe = "clang-format",
+  args = { "--style=file" }
+}
+vim.g.neoformat_enabled_cpp = {"clangformat"}
+vim.g.neoformat_enabled_c = {"clangformat"}
